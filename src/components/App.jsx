7fc02062e445759ef8f2a81b03cead1e66dcb1dd
LBoA <kmfdm69@yahoo.com>
2022-01-25 22:16:30 -0600

    created npm clean command, added rules for coping images to dist, added images to App, added hero scss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import "../styles/index.scss";
 import React from 'react';
 import Recipes from "./Recipes";
+import sword from '../images/swc-sword.png';
+import swordSvg from  "../images/sword.svg";
 
 const App = () => {
     return (
@@ -10,6 +12,8 @@ const App = () => {
             <section>
                 <h1>Here we are.</h1>
             </section>
+            <img src={sword} alt="sword" width="250"/>
+            <img src={swordSvg} alt="sword" width="250"/>
         </main>
         <Recipes />
     </>
--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,25 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   mode: mode,
 
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
+
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
           "css-loader",
           "postcss-loader",
           "sass-loader",
