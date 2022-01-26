3262a3859d6d69ae0de4be489e9c35ebb3269a65
LBoA <kmfdm69@yahoo.com>
2022-01-25 22:44:50 -0600

    Adde clean webpack plugin and move index.html to be generated

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 let mode = "development";
 
 if (process.env.NODE_ENV === "production") {
@@ -9,6 +13,7 @@ module.exports = {
   mode: mode,
 
   output: {
+    path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "images/[hash][ext][query]",
   },
 
@@ -42,7 +47,13 @@ module.exports = {
     ],
   },
 
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 
   resolve: {
     extensions: [".js", ".jsx"],
